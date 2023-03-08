75a2abae9978441a29eda913be541232c38969c9
Christian P. V. Christoffersen <88783438+br-cpvc@users.noreply.github.com>
2023-03-08 14:02:32 +0100

    made it possible to build using original makefile

--- scripts/compile.sh
+++ scripts/compile.sh
@@ -6,6 +6,21 @@ cwd=${script_dir}/..
 builddir=output/build
 
 # make
+USE_ORIGINAL_MAKE=false
+if $USE_ORIGINAL_MAKE; then
+
+cd deps/SVPASEG/src/
+make clean; rm -rf ../bin/; mkdir -p ../bin/
+n=`nproc --ignore=1`
+make -j $n
+cd $cwd
+mkdir -p $builddir/../debian/usr/
+cp -r deps/SVPASEG/bin $builddir/../debian/usr/
+mkdir -p $builddir/../debian/usr/share/svpaseg/
+cp -r deps/SVPASEG/atlasspecs $builddir/../debian/usr/share/svpaseg/
+
+else
+
 mkdir -p $builddir
 cd $builddir
 cmake ../.. \
@@ -18,3 +33,5 @@ cmake ../.. \
 n=`nproc --ignore=1`
 make -j $n install
 cd $cwd
+
+fi
